9c7610baff84a4b4174ab6dea522d053ff9fc9fb
seahailang <seahailang@gmail.com>
2019-04-10 16:44:07 +0800

    decode function

--- decode.py
+++ decode.py
@@ -25,11 +25,16 @@ for line1,line2 in zip(out_file.readlines(),test_file.readlines()):
     pos_list = line2['pos_list']
     spo_list = []
     for r,t in line1.items():
-        spo_list.append({'predicate':relations[r]})
-        spo_list[-1]['subject'] = ''.join([spo_list[i][0] for i in range(t['subject_begin'],t['subject_end']+1)])
-        spo_list[-1]['object'] = ''.join([spo_list[i][0] for i in range(t['object_begin'], t['object_end'] + 1)])
-        spo_list[-1]['subject_type'] = ''
-        spo_list[-1]['object_type'] = ''
+        object_list = t['object']
+        subject_list = t['subject']
+        for obj in object_list:
+            for sbj in subject_list:
+                spo_list.append({'predicate':relations[r]})
+                spo_list[-1]['object'] = ''.join([pos_list[i][0] for i in range(obj[0],obj[1]+1)])
+                spo_list[-1]['subject'] = ''.join([pos_list][0] for i in range(sbj[0],sbj[1]+1))
+                spo_list[-1]['object_type'] = ''
+                spo_list[-1]['subject_type'] = ''
+
     result_file.write({'text':text,'spo_list':spo_list})
 
 out_file.close()
